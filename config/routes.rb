ebad2e0945d3840f82270da649f07f23c6e47930
keisukeadachi <c3a578zx@gmail.com>
2019-06-23 23:27:17 +0900

    add update to ransack app

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/users/search', to: 'users#search'
   get 'users/new', to: 'users#new'
   post 'users/create', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 end
