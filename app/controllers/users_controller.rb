f801abdb9956ea60859ab846a78c95db596eeb75
keisukeadachi <c3a578zx@gmail.com>
2019-06-23 22:24:00 +0900

    add_create_to_ransack_app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
   def search
     user_search = UserSearch.new(params_user_search)
     @users = user_search.execute
@@ -10,5 +18,9 @@ class UsersController < ApplicationController
   def params_user_search
     params.permit(:search_name, :search_age)
   end
-  
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'users#search'
   get '/users/search', to: 'users#search'
+  get 'users/new', to: 'users#new'
+  post 'users/create', to: 'users#create'
 end
